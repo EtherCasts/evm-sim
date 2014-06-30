0288100412ab840f4013ea0d7a539b6867508602
Joris Bontje <jorisbontje@gmail.com>
2014-06-30 16:15:33 +0200

    propose a grant

--- tests/test_mutuala.py
+++ tests/test_mutuala.py
@@ -1,5 +1,5 @@
 from sim import Key, Simulator, compile_serpent
-from pyethereum.utils import coerce_to_bytes, coerce_addr_to_hex
+from pyethereum.utils import coerce_to_bytes, coerce_addr_to_hex, sha3, big_endian_to_int, zpad
 
 import pytest
 
@@ -38,6 +38,28 @@ class TestMutuala(object):
         for idx in range(nr_accounts):
             accounts.append(coerce_addr_to_hex(self.sim.get_storage_data(self.contract, coerce_to_bytes(2**160 + idx))))
         return accounts
+
+    def get_proposal_id(self, proposal):
+        return big_endian_to_int(sha3(zpad(proposal, 32)))
+
+    def get_proposal_recipient(self, proposal):
+        proposal_id = self.get_proposal_id(proposal)
+        return coerce_addr_to_hex(self.sim.get_storage_data(self.contract, coerce_to_bytes(proposal_id + 2**163)))
+
+    def get_proposal_amount(self, proposal):
+        proposal_id = self.get_proposal_id(proposal)
+        return self.sim.get_storage_data(self.contract, coerce_to_bytes(proposal_id + 2**163 + 1))
+
+    def get_proposal_votes(self, proposal):
+        proposal_id = self.get_proposal_id(proposal)
+        return self.sim.get_storage_data(self.contract, coerce_to_bytes(proposal_id + 2**163 + 2))
+
+    def get_proposal_list(self):
+        nr_proposals = self.sim.get_storage_data(self.contract, coerce_to_bytes(2**162-1))
+        proposals = []
+        for idx in range(nr_proposals):
+            proposals.append(coerce_addr_to_hex(self.sim.get_storage_data(self.contract, coerce_to_bytes(2**162 + idx))))
+        return proposals
 
     def test_creation(self):
         assert self.get_account_list() == [self.ALICE.address]
@@ -102,3 +124,75 @@ class TestMutuala(object):
         assert self.get_account_timestamp(self.ALICE.address) == self.sim.genesis.timestamp
         assert self.get_account_tax_credits(self.ALICE.address) == 4106776170
         assert self.get_commons_balance() == 4106776170
+
+    def test_alice_bob_tick(self):
+        ans = self.sim.tx(self.ALICE, self.contract, 0, ["pay", self.BOB.address, 10000000])
+        assert ans == [1]
+
+        self.sim.genesis.timestamp += 30 * 86400
+        ans = self.sim.tx(self.ALICE, self.contract, 0, ["tick"])
+        assert ans == [2]
+
+        assert self.get_account_balance(self.ALICE.address) == 995882766970
+        assert self.get_account_timestamp(self.ALICE.address) == self.sim.genesis.timestamp
+        assert self.get_account_tax_credits(self.ALICE.address) == 4107233030
+
+        assert self.get_account_balance(self.BOB.address) == 9958960
+        assert self.get_account_timestamp(self.BOB.address) == self.sim.genesis.timestamp
+        assert self.get_account_tax_credits(self.BOB.address) == 41040
+
+        assert self.get_commons_balance() == 4107274070
+
+    def test_alice_propose_grant_to_bob(self):
+        ans = self.sim.tx(self.ALICE, self.contract, 0, ["pay", self.BOB.address, 1000])
+        assert ans == [1]
+
+        self.sim.genesis.timestamp += 30 * 86400
+        ans = self.sim.tx(self.ALICE, self.contract, 0, ["tick"])
+        assert ans == [1]
+
+        assert self.get_account_balance(self.ALICE.address) == 995893222780
+        assert self.get_account_timestamp(self.ALICE.address) == self.sim.genesis.timestamp
+        assert self.get_account_tax_credits(self.ALICE.address) == 4106776220
+
+        # Bob doesn't have enough capital to pay tax
+        assert self.get_account_balance(self.BOB.address) == 1000
+        assert self.get_account_timestamp(self.BOB.address) == self.sim.genesis.timestamp - 30 * 86400
+        assert self.get_account_tax_credits(self.BOB.address) == 0
+
+        assert self.get_commons_balance() == 4106776220
+
+        # Propose 5000 grant for Bob to be paid out of Commons
+        proposal_id = self.get_proposal_id("grant to bob")
+        ans = self.sim.tx(self.ALICE, self.contract, 0, ["propose", "grant to bob", self.BOB.address, 5000])
+        assert ans == [proposal_id]
+
+        assert self.get_account_balance(self.ALICE.address) == 995893222780
+        assert self.get_account_timestamp(self.ALICE.address) == self.sim.genesis.timestamp
+        assert self.get_account_tax_credits(self.ALICE.address) == 4106776220
+
+        assert self.get_proposal_recipient("grant to bob") == self.BOB.address
+        assert self.get_proposal_amount("grant to bob") == 5000
+        assert self.get_proposal_votes("grant to bob") == 0
+
+        assert self.get_proposal_list() == [coerce_addr_to_hex(proposal_id)]
+
+    def test_alice_propose_grant_to_nonexisting_account(self):
+        ans = self.sim.tx(self.ALICE, self.contract, 0, ["pay", self.BOB.address, 1000])
+        assert ans == [1]
+
+        self.sim.genesis.timestamp += 30 * 86400
+        ans = self.sim.tx(self.ALICE, self.contract, 0, ["tick"])
+        assert ans == [1]
+
+        ans = self.sim.tx(self.ALICE, self.contract, 0, ["propose", "grant to charlie", self.CHARLIE.address, 5000])
+        assert ans[0] == 0
+        assert coerce_to_bytes(ans[1]) == "invalid to account"
+
+    def test_alice_propose_grant_without_tax_credits(self):
+        ans = self.sim.tx(self.ALICE, self.contract, 0, ["pay", self.BOB.address, 1000])
+        assert ans == [1]
+
+        ans = self.sim.tx(self.BOB, self.contract, 0, ["propose", "grant to alice", self.ALICE.address, 5000])
+        assert ans[0] == 0
+        assert coerce_to_bytes(ans[1]) == "sender has no tax credits"
